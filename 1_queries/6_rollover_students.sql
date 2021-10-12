4bf3472a9bb120f7feb9894b4623e9ea02d6d499
Sophie <sophiejoliver19+1@gmail.com>
2021-10-12 02:09:26 +0000

    add additional queries for student & cohort assignment duration

---
+++ 1_queries/6_rollover_students.sql
@@ -0,0 +1,4 @@
+SELECT students.name as student_name, students.start_date as student_start_date, cohorts.name as cohort_name, cohorts.start_date as cohort_start_date
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohorts.start_date;
