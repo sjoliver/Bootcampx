2097172a61d372f6461c0b93a135ad890ae1964b
Sophie <sophiejoliver19+1@gmail.com>
2021-10-12 02:50:04 +0000

    queries for group-by practice - finding avg assignment duration etc

---
+++ 3_queries_group-by/5_low_average_completion_time.sql
@@ -0,0 +1,7 @@
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+WHERE students.end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration;
