393a2eb5328f100027eb3e994dc175c764b3188c
Sophie <sophiejoliver19+1@gmail.com>
2021-10-14 02:11:57 +0000

    add js files for pool.query

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+// could have connected to the database using Client
+// using a Pool is the preferred way to query with node-postgres
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.query returns a promise 
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+// once this is executed, we are dealing with a JS object (not an SQL query)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  })
+  .catch(err => console.error('query error', err.stack));
